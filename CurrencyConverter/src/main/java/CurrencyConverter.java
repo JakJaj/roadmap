90a37506affbf6267e05bda64d1a27a73f2a3320
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-10-02 12:24:16 +0200

    add web scrapping dependency and create currencies class

--- CurrencyConverter/src/main/java/CurrencyConverter.java
+++ CurrencyConverter/src/main/java/CurrencyConverter.java
@@ -1,5 +1,8 @@
 public class CurrencyConverter {
     public static void main(String[] args) {
-        
+        System.out.println("----    Welcome to the currency converter project    ----");
+
     }
+
+
 }
